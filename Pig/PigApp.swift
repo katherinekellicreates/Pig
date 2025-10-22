c635f120d2b5675504ef495abfd5175612131d37
Kat <katherinekelli.creates@gmail.com>
2025-10-22 10:41:59 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Katherine Pizzirusso on 10/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
